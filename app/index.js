ac3c129a1613975a9f67d6cae32550399ff7c69d
Nil <n_lange@email.com>
2017-12-25 11:41:18 +0100

    initial

---
+++ app/index.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+window.jQuery = $;
+window.lightbox = require('lightbox2');
+require('scripts/main.js');
+require('lightbox2/dist/css/lightbox.css');
+import './sass/main.scss';
---
+++ app/scripts/main.js
@@ -0,0 +1,1 @@
+const alphatier= 111456;
