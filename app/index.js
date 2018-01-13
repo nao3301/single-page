43d38cb99a416f8ed6800541598cfa1a0a47e93b
Nil <n_lange@email.com>
2018-01-13 19:40:21 +0100

    changed font set, added footer

--- app/index.js
+++ app/index.js
@@ -4,4 +4,5 @@ window.lightbox = require('lightbox2');
 
 require('lightbox2/dist/css/lightbox.css');
 import './sass/main.scss';
-import './css/icon-font.css';
+// import './css/icon-font.css';
+import './css/fontawesome/fontawesome-all.css';
