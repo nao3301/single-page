de4d7ac9376f04655c65814ae8847246bcbdc79f
Nil <n_lange@email.com>
2018-02-17 20:11:12 +0100

    added popup component

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,7 +6,19 @@ const lib = {
   },
   closeWindow: function (e,ref){
     document.getElementById(ref).style.display = 'none';
-  }
+  },
+  openPopup: function (e,ref){
+    document.getElementById(ref).style.visibility = 'visible';
+    document.getElementById(ref).style.opacity = 1;
+  },
+  closePopup: function (e,ref){
+    e.stopPropagation();
+    document.getElementById(ref).style.visibility = 'hidden';
+    document.getElementById(ref).style.opacity = 0;
+  },
+  childClick: function (e,ref){
+    e.stopPropagation();
+  },
 };
 
 window.lib = lib;
