afec583b7908b223c4ef3f5ae9d50e80a228081c
Nil <n_lange@email.com>
2018-03-04 20:16:25 +0100

    3 different image galleries -> todo change ratios and images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -23,7 +23,85 @@ const lib = {
   childClick: function (e, ref) {
     e.stopPropagation();
   },
-  initGallery: function () {
+  initPortraitGallery: function () {
+
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+      {
+        src: 'img/portraits/LR_-0349.jpg',
+        w: 4000,
+        h: 6000
+      },
+      {
+        src: 'img/portraits/LR_-7901.jpg',
+        w: 4016,
+        h: 6016
+
+      },
+      {
+        src: 'img/portraits/rici-7991.jpg',
+        w: 4016,
+        h: 6016
+      }
+    ];
+
+    // define options (if needed)
+    var options = {
+      // optionName: 'option value'
+      // for example:
+      bgOpacity: 0.8,
+      index: 0, // start at first slide
+      spacing: 0.02,
+      clickToCloseNonZoomable: false,
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+
+  },
+  initAnimalGallery: function () {
+
+    var pswpElement = document.querySelectorAll('.pswp')[0];
+
+    // build items array
+    var items = [
+      {
+        src: 'img/tiere/LR_-0060.jpg',
+        w: 4000,
+        h: 6000
+      },
+      {
+        src: 'img/tiere/LR_-0893.jpg',
+        w: 4016,
+        h: 6016
+
+      },
+      {
+        src: 'img/portraits/rici-7991.jpg',
+        w: 4016,
+        h: 6016
+      }
+    ];
+
+    // define options (if needed)
+    var options = {
+      // optionName: 'option value'
+      // for example:
+      bgOpacity: 0.8,
+      index: 0, // start at first slide
+      spacing: 0.02,
+      clickToCloseNonZoomable: false,
+    };
+
+    // Initializes and opens PhotoSwipe
+    var gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, items, options);
+    gallery.init();
+
+  },
+  initPassionGallery: function () {
 
     var pswpElement = document.querySelectorAll('.pswp')[0];
 
